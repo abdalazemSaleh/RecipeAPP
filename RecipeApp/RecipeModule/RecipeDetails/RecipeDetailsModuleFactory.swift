95f91b37c697dcd4ad47a087c741d4f2dd3e8912
AbdalazemSaleh <Abdalazem.saleh2@icloud.com>
2024-08-13 18:41:48 +0300

    Add main folders for Recipe Module.📂

---
+++ RecipeApp/RecipeModule/RecipeDetails/RecipeDetailsModuleFactory.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeDetailsModuleFactory.swift
+//  RecipeApp
+//
+//  Created by Abdalazem Saleh on 13/08/2024.
+//
+
+import Foundation
---
+++ RecipeApp/RecipeModule/Recipes/RecipesModuleFactory.swift
@@ -0,0 +1,8 @@
+//
+//  RecipesModuleFactory.swift
+//  RecipeApp
+//
+//  Created by Abdalazem Saleh on 13/08/2024.
+//
+
+import Foundation
