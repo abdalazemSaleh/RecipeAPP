e30d9dfeb70c7a3975422a887e736a04efae2de0
AbdalazemSaleh <Abdalazem.saleh2@icloud.com>
2024-08-15 12:31:52 +0300

    Make apearance lgiht mode only

--- RecipeApp/RecipeModule/RecipeDetails/UserInterface/View/RecipeDetailsViewController.swift
+++ RecipeApp/RecipeModule/RecipeDetails/UserInterface/View/RecipeDetailsViewController.swift
@@ -54,12 +54,6 @@ class RecipeDetailsViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
     // MARK: - FUNCTIONS
